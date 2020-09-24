0c82050c28119f189ed18e35e5b32851f93dfdfc
misael-macAir <hola@landercorp.mx>
2020-09-24 12:11:43 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Francisco Misael Landero Ychante on 24/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp.swift
+//  Shared
+//
+//  Created by Francisco Misael Landero Ychante on 24/09/20.
+//
+
+import SwiftUI
+
+@main
+struct newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
